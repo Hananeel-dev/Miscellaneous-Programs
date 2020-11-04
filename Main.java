04eaec3707325a14d90ce0618592efe7e89f36ec
Hananeel-dev <73926172+Hananeel-dev@users.noreply.github.com>
2020-11-04 12:00:05 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	  String input = "character";
+        char[] array = input.toCharArray();//for easily comparing character with char array
+        char choice = 'h';
+
+
+        int b = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (choice == array[i]) {
+                b++;
+                continue;
+            } else
+                continue;
     }
 }
